0b8871524d386a7f8db8647dbc4ae1a485435502
Tejaswi <161420676+jampanitejaswi@users.noreply.github.com>
2024-08-30 10:00:40 +0530

    Create arthematic.py

---
+++ arthematic.py
@@ -0,0 +1,10 @@
+def arithmetic(a,b):
+    print("The Result : ")
+    print("Addition= ",a+b)
+    print("Subtraction= ",a-b)
+    print("Multiplication= ",a*b)
+    print("Division= ",a/b)            
+    print("Floor Division= ",a//b)
+    print("Modulo Division= ",a%b)
+arithmetic(10,5)
+        
