c74bc91aa244ebfdec441f782ebe87fde2263539
Tejaswi <161420676+jampanitejaswi@users.noreply.github.com>
2024-08-30 10:18:44 +0530

    Create rev.py

---
+++ rev.py
@@ -0,0 +1,10 @@
+def func(a,b):
+    add=a+b
+    sub=a-b
+    mul=a*b
+    return add,sub,mul
+result= func(9,5)    
+add,sub,mul=result
+print("addition: ",add)
+print("subtract: ",sub)
+print("multiply: ",mul)
