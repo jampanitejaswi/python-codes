ae0937086daea9806779e1af490565a2a222f8af
Tejaswi <161420676+jampanitejaswi@users.noreply.github.com>
2024-08-30 10:14:53 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,11 @@
+l1=[]
+n=int(input("enter no of elements: "))
+for i in range(n):
+     ele=int(input())
+     l1.append(ele)
+print(l1) 
+l2=[]
+n=int(input("enter no of elements: "))
+for i in range(n):
+     ele=int(input())
+     l2.append(ele)
